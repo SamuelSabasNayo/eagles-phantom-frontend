dbcd5bfbff86423088ec17ee42ca3fe94fe432e0
Mugema123 <mugemaleo@gmail.com>
2020-11-13 07:14:45 +0200

    ch(setupTest):setup-testing-environment-with-jest-and-enzyme

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
 import React,{ Component } from 'react';
+import UserLogout from './logout/Logout';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../redux/actions/index';
 
-class App extends Component{
-  render(){
+// 
+
+class App extends Component {
+  componentDidMount () {
+    this.props.onTryAutoSignup(); 
+  }
+
+  render () {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if ( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={UserLogout} /> 
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div>
-        <h1>Hello!! Welcome to phantom App.</h1>
+        <h1>UserLogout</h1>
+        <Layout>
+          {UserLogout}
+          {routes}
+        </Layout>
       </div>
     );
   }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
 };
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch( actions.authCheckState() )
+  };
+};
+
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export {
+    logout,
+    setAuthRedirectPath,
+    authCheckState
+} from './auth';
